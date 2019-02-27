bfa964b61d586f57279795da2ade90f76bc39d1a
Habenzy <robertrstauss@gmail.com>
2019-02-27 11:51:49 -0500

    changed structure of all galleries to use modal zoom, minor css changes

--- client/src/App/pages/eventgallery.js
+++ client/src/App/pages/eventgallery.js
@@ -9,16 +9,33 @@ class EventGallery extends React.Component {
 
    componentDidMount() {
     const gallery = document.getElementById("photo-gallery");
-    let imageSrcs = [];
-    //a better way to do this would be to iterate over images dir and auto generate imgSrcs array
+    let imageNames = ['Corner-School-Float-photo',];
+    let thumbElements = []
 
-    for (let src of imageSrcs) {
+    
+    for (let name of imageNames) {
       gallery.innerHTML += (
         `<div class="image-thumb">
-          <a href=${"./images/events/" + src}><img class="thumb" src=${"./images/events/" + src} alt=""/></a>
+          <img class="thumb" id=${name} src=${"/images/events/" + name + ".jpg"} alt="" />
+        </div>
+        <div id=${"modal-" + name} class="modal">
+          <span class="close" id=${"close-" + name}>&times;</span>
+          <img class="modal-content" id=${"img-" + name} src=${"/images/events/" + name + ".jpg"}>
         </div>`
-      )
-    }
+      );
+      thumbElements.push(document.getElementById(name));
+    };
+
+    thumbElements.forEach( function(img){
+      let modal = document.getElementById("modal-" + img.id);
+      console.log(modal);
+      document.getElementById(img.id).onclick = function () {
+        modal.style.display = "block";
+      };
+      document.getElementById("close-" + img.id).onclick = function () {
+        modal.style.display = "none"
+      }
+    })
   }
 
   render() {
--- client/src/App/pages/restorationgallery.js
+++ client/src/App/pages/restorationgallery.js
@@ -8,15 +8,32 @@ class RestorationGallery extends React.Component {
 
    componentDidMount() {
     const gallery = document.getElementById("photo-gallery");
-    let imageNames = ['Corner-School-Float-photo', 'Corner-School-looking-South', 'Eleanor-and-Kate', 'Eleanor', 'Granville_91207-039', 'Granville_91207-044', 'Granville_91207-014', 'IMG_1757', 'IMG_1758', 'IMG_1760', 'IMG_7376'];
-
+    let imageNames = ['Corner-School-looking-South', 'Eleanor-and-Kate', 'Eleanor', 'Granville_91207-039', 'Granville_91207-044', 'Granville_91207-014', 'IMG_1757', 'IMG_1758', 'IMG_1760', 'IMG_7376'];
+    let thumbElements =[];
+    
     for (let name of imageNames) {
       gallery.innerHTML += (
         `<div class="image-thumb">
-          <a href=${"./images/restoration/" + name + ".jpg"}><img class="thumb" src=${"./images/restoration/" + name + ".jpg"} alt=""/></a>
+          <img class="thumb" id=${name} src=${"/images/restoration/" + name + ".jpg"} alt="" />
+        </div>
+        <div id=${"modal-" + name} class="modal">
+          <span class="close" id=${"close-" + name}>&times;</span>
+          <img class="modal-content" id=${"img-" + name} src=${"/images/restoration/" + name + ".jpg"}>
         </div>`
-      )
-    }
+      );
+      thumbElements.push(document.getElementById(name));
+    };
+
+    thumbElements.forEach( function(img){
+      let modal = document.getElementById("modal-" + img.id);
+      console.log(modal);
+      document.getElementById(img.id).onclick = function () {
+        modal.style.display = "block";
+      };
+      document.getElementById("close-" + img.id).onclick = function () {
+        modal.style.display = "none"
+      }
+    })
   }
 
   render() {
