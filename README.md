2c87be3b083eed5f24204f616d304eb679db3226
Habenzy <robertrstauss@gmail.com>
2019-03-19 19:45:07 -0400

    removed unused code, and updated README

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Welcome to the Granville Corner School Resource Center Web App
 
+The Corner School Resource Center of Granvile, VT is dedicated to restoring the upper village "Corner School" one room school house, and transforming it into a technological resource center for residents, and a repository of town history.
+
 Audio files are hosted on, and served by Soundcloud
 
-Images are hosted on and served by the site itself, and are located in ./client/public/images/{the corresponding folder}
-photos used on non-gallery pages are located in the top level photo directory.
+Images are hosted on and served by the site itself, and are located in ./client/public/images/{the corresponding folder}, and can be accessed through ./images/{directory}/{file name}, and their names must be added to the imageNames array in the componentDidMount function in their corresponding galleries to include them in the gallery. Photos used on non-gallery pages are located in the top level photo directory and can be accessed at ./photos/{file name}.
 
-The Corner School Resource Center of Granvile, VT is dedicated to restoring the upper village "Corner School" one room school house, and transforming it into a technological resource center for residents, and a repository of town history.
+Quotes and events to be displayed on the main page can be added by putting them (as a string) in the quotes array located at client/src/App/pages/home.js in the displayQuotes function.
--- client/src/App/pages/home.js
+++ client/src/App/pages/home.js
@@ -4,18 +4,13 @@ import Header from './header';
 
 class Home extends React.Component {
 
-  getQuote() {
-    return this.state.quotes[Math.floor(Math.random() * (this.state.quotes.length))]
-  }
-
   displayQuotes() {
     const displayArea = document.getElementById('event-quote');
     const quotes = [`There was no Route 100 until 1912 up through the Gulf. The main route was Puddledock.  -Norm A.`, `We were kind of free spirits, so we decided to move here and see how it was, have a garden. And here we are 43 years later.  -Nancy D.`, `There was a tree that was snowing, but there wasn't snow anywhere else. It felt like a blessing.  -Michael E.`, `No traffic lights, no stop signs, no cops, no buses, no trains. I just kind of fell in love with it. Living up in the hollows, up on the hills is an adventure.  -Jamie S.`, `Larry sold me a parcel. Started building in 1966. Every nail, every wire, I put in.  -Fritz B.`]
 
     displayArea.innerHTML = quotes[Math.floor(Math.random() * (quotes.length))]
-    window.setInterval(function(){
+    window.setInterval(function () {
       displayArea.textContent = quotes[Math.floor(Math.random() * (quotes.length))];
-      
     }, 10000)
   }
 
@@ -30,7 +25,7 @@ class Home extends React.Component {
         <Header />
         <div id="home-body">
           <div id="cover">
-            <img id="home-img" src="./images/50.jpeg" alt="The Granville Corner School"/>
+            <img id="home-img" src="./images/50.jpeg" alt="The Granville Corner School" />
             <div id="mission-statement">
               <p>The Corner School Resource Center of Granville is dedicated to<br /> the preservation of the region's unique natural and cultural history, and the creation of a learning center that connects Granville to the wider world in the historic Corner School building.</p>
             </div>
