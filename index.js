0b0781ea3579f84e0e244568cf07eeec87988295
Habenzy <robertrstauss@gmail.com>
2019-03-19 19:26:39 -0400

    now with scrolling random quotes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
