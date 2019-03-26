7013f984e2e7b68dff3ab9aae337e5db8af5dd14
Habenzy <robertrstauss@gmail.com>
2019-03-26 10:12:13 -0400

    now with working donate button

--- client/src/App/pages/footer.js
+++ client/src/App/pages/footer.js
@@ -17,7 +17,13 @@ class Footer extends React.Component {
           <button id='contact'>Contact Us</button>
           <div>75 Post Office Rd. Granville, VT 05747</div>
           <div><a href="https://www.facebook.com/CornerSchoolResourceCenterOfGranvilleVt/">FaceBook</a></div>
-          <div>Donate</div>
+          <div><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_donations" />
+<input type="hidden" name="business" value="WMV8PDEYN8D4E" />
+<input type="hidden" name="currency_code" value="USD" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form></div>
         </div>
     )
   }
