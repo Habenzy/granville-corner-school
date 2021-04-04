baf8a915e45f8323e1428c7e31912f8f3e4bbd6b
Robert Stauss <robertrstauss@gmail.com>
2021-04-04 12:54:46 -0400

    bringing back the quotes

--- client/src/App/pages/home.js
+++ client/src/App/pages/home.js
@@ -7,12 +7,12 @@ class Home extends React.Component {
   displayQuotes() {
     const displayArea = document.getElementById('event-quote');
     const quotes = [
-      // `There was no Route 100 until 1912 up through the Gulf. The main route was Puddledock.  -Norm A.`,
-      // `We were kind of free spirits, so we decided to move here and see how it was, have a garden. And here we are 43 years later.  -Nancy D.`,
-      // `There was a tree that was snowing, but there wasn't snow anywhere else. It felt like a blessing.  -Michael E.`,
-      // `No traffic lights, no stop signs, no cops, no buses, no trains. I just kind of fell in love with it. Living up in the hollows, up on the hills is an adventure.  -Jamie S.`,
-      // `Larry sold me a parcel. Started building in 1966. Every nail, every wire, I put in.  -Fritz B.`,
-      "We're just $5000 away from opening in the summer of 2021! Every Dollar Counts! Help us build community by donating today. And thanks!"
+      `There was no Route 100 until 1912 up through the Gulf. The main route was Puddledock.  -Norm A.`,
+      `We were kind of free spirits, so we decided to move here and see how it was, have a garden. And here we are 43 years later.  -Nancy D.`,
+      `There was a tree that was snowing, but there wasn't snow anywhere else. It felt like a blessing.  -Michael E.`,
+      `No traffic lights, no stop signs, no cops, no buses, no trains. I just kind of fell in love with it. Living up in the hollows, up on the hills is an adventure.  -Jamie S.`,
+      `Larry sold me a parcel. Started building in 1966. Every nail, every wire, I put in.  -Fritz B.`,
+      // "We're just $5000 away from opening in the summer of 2021! Every Dollar Counts! Help us build community by donating today. And thanks!"
     ]
 
     displayArea.innerHTML = quotes[Math.floor(Math.random() * (quotes.length))]
