8332e0a9f7a5207dbafee4b5954f98a4ee1ddcc9
Bob Stauss <rstauss@burlingtoncodeacademy>
2021-11-06 13:09:30 -0400

    standard front end image

--- client/src/App/pages/home.js
+++ client/src/App/pages/home.js
@@ -12,7 +12,6 @@ class Home extends React.Component {
       `There was a tree that was snowing, but there wasn't snow anywhere else. It felt like a blessing.  -Michael E.`,
       `No traffic lights, no stop signs, no cops, no buses, no trains. I just kind of fell in love with it. Living up in the hollows, up on the hills is an adventure.  -Jamie S.`,
       `Larry sold me a parcel. Started building in 1966. Every nail, every wire, I put in.  -Fritz B.`,
-      // "We're just $5000 away from opening in the summer of 2021! Every Dollar Counts! Help us build community by donating today. And thanks!"
     ]
 
     displayArea.innerHTML = quotes[Math.floor(Math.random() * (quotes.length))]
@@ -32,7 +31,7 @@ class Home extends React.Component {
         <Header />
         <div id="home-body">
           <div id="cover">
-            <img id="home-img" src="./images/poster.jpeg" alt="The Granville Corner School" />
+            <img id="home-img" src="./images/50.jpeg" alt="The Granville Corner School" />
             <div id="mission-statement">
               <p>The Corner School Resource Center of Granville is dedicated to<br /> the preservation of the region's unique natural and cultural history, and the creation of a learning center that connects Granville to the wider world in the historic Corner School building.</p>
             </div>
