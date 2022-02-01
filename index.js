3ec8c42940cb9f26e1b874501335b3726aa5e961
habenzy <rstauss@burlingtoncodeacademy.com>
2022-02-01 10:24:05 -0500

    cleaning house

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-
+const port = process.env.PORT || 5000;
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
+app.listen(port, () => {
+  console.log("App is listening on port " + port);
+});
