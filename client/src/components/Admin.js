2a0521be4a43939623762148f3519a64ebc43c8d
habenzy <rstauss@burlingtoncodeacademy.com>
2022-02-26 13:09:51 -0500

    image upload functionality

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,11 +1,8 @@
 import { setDoc, doc } from "firebase/firestore";
-import { db } from "../config/firebase";
+import { db, storage } from "../config/firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { useState, useEffect } from "react";
 import { setUserProperties } from "firebase/analytics";
-
-async function createGalleryEntry(entry, gallery) {
-  await setDoc(doc(db, gallery, entry.name), entry);
-}
 
 async function createEventEntry(entry) {
   await setDoc(doc(db, "events", entry.name), entry);
@@ -14,23 +11,52 @@ async function createEventEntry(entry) {
 export default function Admin(props) {
   const [name, setName] = useState("");
   const [blurb, setBlurb] = useState("");
-  const [gallery, setGallery] = useState("");
-  const [imagePath, setImagePath] = useState("");
+  const [gallery, setGallery] = useState("history");
+  const [imageFile, setImageFile] = useState("");
+
+  async function createGalleryEntry() {
+    let galleryRef = ref(storage, gallery);
+    console.log(galleryRef);
+    let imgRef = ref(galleryRef, imageFile.name);
+    console.log(imgRef);
+    const uploadTask = uploadBytesResumable(imgRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot);
+      },
+      (err) => {
+        //catches the errors
+        console.log(err);
+      },
+      () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          let entry = {
+            name: name,
+            blurb: blurb,
+            url: downloadURL,
+          };
+          console.log(db, gallery, entry.name)
+          setDoc(doc(db, gallery, entry.name), entry);
+        });
+      }
+    );
+  }
 
   return (
     <div id="admin-body">
-      <form id="gallery-form" onSubmit={(evt) => {
-        evt.preventDefault()
-        let dbEntry = {
-          name: name,
-          blurb: blurb,
-          url: imagePath,
-          gallery: gallery
-        }
-
-        console.log(dbEntry)
-      }}>
-        <label for="file-name">Image name</label>
+      <form
+        id="gallery-form"
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          createGalleryEntry();
+        }}
+      >
+        <label htmlFor="file-name">Image name</label>
         <input
           onChange={(evt) => {
             setName(evt.target.value);
@@ -41,7 +67,9 @@ export default function Admin(props) {
           type="text"
           placeholder="Image name..."
         />
-        <label for="blurb">Description that will appear under the image</label>
+        <label htmlFor="blurb">
+          Description that will appear under the image
+        </label>
         <input
           onChange={(evt) => {
             setBlurb(evt.target.value);
@@ -52,7 +80,7 @@ export default function Admin(props) {
           type="text"
           placeholder="Image blurb..."
         />
-        <label for="gallery-choice">
+        <label htmlFor="gallery-choice">
           Choose the Gallery you want this image to appear in
         </label>
         <select
@@ -67,18 +95,17 @@ export default function Admin(props) {
           <option value="restoration">Restoration Gallery</option>
           <option value="event">Event Gallery</option>
         </select>
-        <label for="image">Choose an Image to upload</label>
+        <label htmlFor="image">Choose an Image to upload</label>
         <input
           onChange={(evt) => {
-            setImagePath(evt.target.files[0]);
+            setImageFile(evt.target.files[0]);
           }}
           required={true}
           name="image"
           type="file"
           accept="image/png, image/jpeg"
         />
-
-        <input type="submit" value="Submit Photo"/>
+        <input type="submit" value="Submit Photo" />
       </form>
     </div>
   );
--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+import {getStorage} from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,8 +22,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const analytics = getAnalytics(app);
+const storage = getStorage(app)
 
-export {db, app}
+export {db, app, storage}
 
 // let gallerySetup = [
   
