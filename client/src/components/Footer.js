48d12f14e80555c79f33f8dda101ce45d5476ea0
habenzy <rstauss@burlingtoncodeacademy.com>
2023-08-25 14:11:19 -0400

    updated donate button

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -21,28 +21,8 @@ function Footer(props) {
         </div>
       </a>
       <div>
-        <form
-          action="https://www.paypal.com/donate"
-          method="post"
-          target="_top"
-        >
-          <input type="hidden" name="hosted_button_id" value="HDFJNE3K38H2Y" />
-          <input
-            type="image"
-            src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-            border="0"
-            name="submit"
-            title="PayPal - The safer, easier way to pay online!"
-            alt="Donate with PayPal button"
-          />
-          <img
-            alt=""
-            border="0"
-            src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-            width="1"
-            height="1"
-          />
-        </form>
+        
+        <a href="https://www.paypal.com/donate/?hosted_button_id=BC64TJ78HVM9Y" target="_blank" ><button className="nav-button" >Donate</button></a>
       </div>
     </div>
   );
