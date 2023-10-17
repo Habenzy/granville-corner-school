5cd741c85ed248508541cf27bc133f46e1422db3
habenzy <rstauss@burlingtoncodeacademy.com>
2023-10-17 09:38:32 -0400

    events over

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
-import Event from "./Event"
+import Event from "./Event";
 
 function Quote(props) {
   const [fade, setFade] = useState("fade-in");
 
   useEffect(() => {
     const timeout = setInterval(() => {
-      setFade("fade-out")
+      setFade("fade-out");
     }, 9000);
     return () => clearInterval(timeout);
   }, [fade]);
 
-  return(
-    <div id="event-quote" className={fade} onTransitionEnd={() => setFade("fade-in")}>{props.quote}</div>
-  )
+  return (
+    <div
+      id="event-quote"
+      className={fade}
+      onTransitionEnd={() => setFade("fade-in")}
+    >
+      {props.quote}
+    </div>
+  );
 }
 
 function Home(props) {
@@ -28,10 +34,10 @@ function Home(props) {
     "Larry sold me a parcel. Started building in 1966. Every nail, every wire, I put in.  -Fritz B."
   );
   const [announcements, setAnnouncements] = useState([
-    {title: "Art Show: Made in the Mountains", blurb: "Featuring the work of local artists Clare Walker Leslie, Johno Landsman, Samantha Sheehan, Phil Bobrow, Meg Reinhold and Clayton Anderson. Music by Granville Daze!", time: "Open Saturdays & Sundays through October 15"},
-    {title: "Story Hour for Kids Kick-off! ", blurb: "Stories, songs, games and snacks for small folk and their caregivers. Register to get free children’s books sent directly to your home through  Dolly Parton’s Imagination Library. ", time: "Sunday, Sept. 24 at 1pm"},
-    {title: "A Celebration of Granville History", blurb: "Watch “A Hill Farmer’s Story” a short documentary film based on the diaries of early 20th century Granville residents. Sign up for our Oral History project and share a story of your own!", time: "Sunday, Oct. 15 at 7pm"}
-  ])
+    // {title: "Art Show: Made in the Mountains", blurb: "Featuring the work of local artists Clare Walker Leslie, Johno Landsman, Samantha Sheehan, Phil Bobrow, Meg Reinhold and Clayton Anderson. Music by Granville Daze!", time: "Open Saturdays & Sundays through October 15"},
+    // {title: "Story Hour for Kids Kick-off! ", blurb: "Stories, songs, games and snacks for small folk and their caregivers. Register to get free children’s books sent directly to your home through  Dolly Parton’s Imagination Library. ", time: "Sunday, Sept. 24 at 1pm"},
+    // {title: "A Celebration of Granville History", blurb: "Watch “A Hill Farmer’s Story” a short documentary film based on the diaries of early 20th century Granville residents. Sign up for our Oral History project and share a story of your own!", time: "Sunday, Oct. 15 at 7pm"}
+  ]);
 
   useEffect(() => {
     let timeoutID = setInterval(() => {
@@ -47,22 +53,26 @@ function Home(props) {
     <div>
       <div id="home-body">
         <div id="cover">
-          {/* <div id="mission-statement">
+          <div id="mission-statement">
             <p>
               The Corner School Resource Center of Granville is dedicated to the
               preservation of the region's unique natural and cultural history,
               and the creation of a learning center that connects Granville to
               the wider world in the historic Corner School building.
             </p>
-          </div> */}
+          </div>
         </div>
         <div className="event-display">
           {announcements.map((announce, i) => {
-            return <div key={i} className="event-item">
-              <h3>{announce.title}</h3>
-              <p>{announce.blurb}</p>
-              <p><b>{announce.time}</b></p>
-            </div>
+            return (
+              <div key={i} className="event-item">
+                <h3>{announce.title}</h3>
+                <p>{announce.blurb}</p>
+                <p>
+                  <b>{announce.time}</b>
+                </p>
+              </div>
+            );
           })}
         </div>
         <Quote quote={dispQuote} />
